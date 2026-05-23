bcfce3d44e7c348af89ecfc314a8cced5dc92aba
eldar6776~ <34648417+eldar6776@users.noreply.github.com>
2026-05-23 10:06:26 +0200

    Update HotelRoomThermostat project files and resources
    - Changed uiExportFolderPath in HotelRoomThermostat.slp to reflect new directory structure.
    - Updated various properties in HotelRoomThermostat.spj, including position and alignment values, label text, and added hidden properties for labels and screens.
    - Modified selected_screen and backup count in project info.
    - Added new images (workaround1.jpg and workaround2.jpg) to documentation.
    - Updated binary backup files indicating changes in their contents.

--- fw/include/version.h
+++ fw/include/version.h
@@ -3,7 +3,7 @@
 
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
-#define VERSION_PATCH 204
-#define BUILD_DATE "2026-05-23 10:04:52"
+#define VERSION_PATCH 205
+#define BUILD_DATE "2026-05-23 10:06:08"
 
 #endif
